d6a2eb01a97af4067505666525b3888ff3dbb5fd
Ramadhan <ramiqcom@gmail.com>
2023-09-27 21:09:19 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Add Express
+import express from 'express';
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
+
+// Export the Express API
+module.exports = app;
