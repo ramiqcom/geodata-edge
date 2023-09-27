4502fb5ce06e892dc2d4c8dac6187bdae521552a
Ramadhan <ramiqcom@gmail.com>
2023-09-27 21:34:19 +0700

    change route

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-// index.js
+// Import packages
 import express from 'express';
+import dotenv from 'dotenv';
 
-const app = express()
-const PORT = 4000
+// Run dotenv
+dotenv.config();
 
+// Prepare the app
+const app = express();
+const PORT = 3000;
+
+// Make the app listen to the port
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
-})
+});
 
+// Main rout
 app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
-
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
+  res.send('Welcome to Geo-Edge')
 });
